da60ca0ea237a412f5b87e35c9c5f1cdc9e85cd0
tausiftt52 <tausiftt5238@gmail.com>
2019-11-19 01:29:39 -0500

    another test

--- face_detect/views.py
+++ face_detect/views.py
@@ -20,5 +20,3 @@ def move_machine(request):
     # machine.write('G0 X0 Y0\n'.encode())
     print('moved by x: ' + x + ', y: ' + y)
     return HttpResponse('moved by x: ' + x + ', y: ' + y)
-
-    #For testing
